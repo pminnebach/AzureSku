43b735d9b4fb3cb9b24c8dcfb02a09f9da2dc72d
pminnebach <philip@minnebach.be>
2019-04-13 22:42:20 +0200

    Updated pscustomobject to ordered hashtable.

--- AzureSku.ps1
+++ AzureSku.ps1
@@ -1,44 +1,44 @@
 $Skus = @(
-    [pscustomobject]@{
+    [ordered]@{
         SkuId = '0c266dff-15dd-4b49-8397-2bb16070ed52'
         SkuPartNumber = 'MCOMEETADV'
         SkuFriendlyName = 'AUDIO CONFERENCING'
         ServicePlans = @(
-            [pscustomobject]@{
+            [ordered]@{
                 SkuId = '3e26ee1f-8a5f-4d52-aee2-b81ce45c8f40'
                 SkuPartNumber = 'MCOMEETADV'
                 SkuFriendlyName = 'AUDIO CONFERENCING'
             }
         )
     },
-    [pscustomobject]@{
+    [ordered]@{
         SkuId = '2b9c8e7c-319c-43a2-a2a0-48c5c6161de7'
         SkuPartNumber = 'AZURE ACTIVE DIRECTORY BASIC'
         SkuFriendlyName = 'AAD_BASIC'
         ServicePlans = @(
-            [pscustomobject]@{
+            [ordered]@{
                 SkuId = 'c4da7f8a-5ee2-4c99-a7e1-87d2df57f6fe'
                 SkuPartNumber = 'AAD_BASIC'
                 SkuFriendlyName = 'MICROSOFT AZURE ACTIVE DIRECTORY BASIC'
             }
         )
     },
-    [pscustomobject]@{
+    [ordered]@{
         SkuId = '078d2b04-f1bd-4111-bbd4-b4b1b354cef4'
         SkuPartNumber = 'AZURE ACTIVE DIRECTORY PREMIUM P1'
         SkuFriendlyName = 'AAD_PREMIUM'
         ServicePlans = @(
-            [pscustomobject]@{
+            [ordered]@{
                 SkuId = '41781fb2-bc02-4b7c-bd55-b576c07bb09d'
                 SkuPartNumber = 'AAD_PREMIUM'
                 SkuFriendlyName = 'AZURE ACTIVE DIRECTORY PREMIUM P1'
             },
-            [pscustomobject]@{
+            [ordered]@{
                 SkuId = '932ad362-64a8-4783-9106-97849a1a30b9'
                 SkuPartNumber = 'ADALLOM_S_DISCOVERY'
                 SkuFriendlyName = 'CLOUD APP SECURITY DISCOVERY'
             },
-            [pscustomobject]@{
+            [ordered]@{
                 SkuId = '8a256a2b-b617-496d-b51b-e76466e88db0'
                 SkuPartNumber = 'MFA_PREMIUM'
                 SkuFriendlyName = 'MICROSOFT AZURE MULTI-FACTOR AUTHENTICATION'
@@ -47,5 +47,5 @@ $Skus = @(
     }
 )
 
-$OutPathJson = Join-Path -Path $PSScriptRoot -ChildPath 'AzureSku.json'
-$Skus | ConvertTo-Json | Out-File -FilePath $OutPathJson
+$OutPathJson = Join-Path -Path $PWD -ChildPath 'AzureSku.json'
+$Skus | ConvertTo-Json -Depth 3 | Out-File -FilePath $OutPathJson
