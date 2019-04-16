b25d36fb27da543cf976b9d48cfefbebd4f734ef
Minnebach Philip <Philip.minnebach@hestia-it.be>
2019-04-16 08:41:52 +0200

    Added skus.

--- AzureSku.ps1
+++ AzureSku.ps1
@@ -1,51 +1,172 @@
 $Skus = @(
     [ordered]@{
-        SkuId = '0c266dff-15dd-4b49-8397-2bb16070ed52'
-        SkuPartNumber = 'MCOMEETADV'
+        SkuId           = '0c266dff-15dd-4b49-8397-2bb16070ed52'
+        SkuPartNumber   = 'MCOMEETADV'
         SkuFriendlyName = 'AUDIO CONFERENCING'
-        ServicePlans = @(
+        ServicePlans    = @(
             [ordered]@{
-                ServicePlanId = '3e26ee1f-8a5f-4d52-aee2-b81ce45c8f40'
-                ServicePlanName = 'MCOMEETADV'
+                ServicePlanId           = '3e26ee1f-8a5f-4d52-aee2-b81ce45c8f40'
+                ServicePlanName         = 'MCOMEETADV'
                 ServicePlanFriendlyName = 'AUDIO CONFERENCING'
             }
         )
-    },
+    }
     [ordered]@{
-        SkuId = '2b9c8e7c-319c-43a2-a2a0-48c5c6161de7'
-        SkuPartNumber = 'AZURE ACTIVE DIRECTORY BASIC'
-        SkuFriendlyName = 'AAD_BASIC'
-        ServicePlans = @(
+        SkuId           = '2b9c8e7c-319c-43a2-a2a0-48c5c6161de7'
+        SkuPartNumber   = 'AAD_BASIC'
+        SkuFriendlyName = 'AZURE ACTIVE DIRECTORY BASICAAD_BASIC'
+        ServicePlans    = @(
             [ordered]@{
-                ServicePlanId = 'c4da7f8a-5ee2-4c99-a7e1-87d2df57f6fe'
-                ServicePlanName = 'AAD_BASIC'
+                ServicePlanId           = 'c4da7f8a-5ee2-4c99-a7e1-87d2df57f6fe'
+                ServicePlanName         = 'AAD_BASIC'
                 ServicePlanFriendlyName = 'MICROSOFT AZURE ACTIVE DIRECTORY BASIC'
             }
         )
-    },
+    }
     [ordered]@{
-        SkuId = '078d2b04-f1bd-4111-bbd4-b4b1b354cef4'
-        SkuPartNumber = 'AZURE ACTIVE DIRECTORY PREMIUM P1'
+        SkuId           = '078d2b04-f1bd-4111-bbd4-b4b1b354cef4'
+        SkuPartNumber   = 'AZURE ACTIVE DIRECTORY PREMIUM P1'
         SkuFriendlyName = 'AAD_PREMIUM'
-        ServicePlans = @(
+        ServicePlans    = @(
             [ordered]@{
-                ServicePlanId = '41781fb2-bc02-4b7c-bd55-b576c07bb09d'
-                ServicePlanName = 'AAD_PREMIUM'
+                ServicePlanId           = '41781fb2-bc02-4b7c-bd55-b576c07bb09d'
+                ServicePlanName         = 'AAD_PREMIUM'
                 ServicePlanFriendlyName = 'AZURE ACTIVE DIRECTORY PREMIUM P1'
-            },
+            }
             [ordered]@{
-                ServicePlanId = '932ad362-64a8-4783-9106-97849a1a30b9'
-                ServicePlanName = 'ADALLOM_S_DISCOVERY'
+                ServicePlanId           = '932ad362-64a8-4783-9106-97849a1a30b9'
+                ServicePlanName         = 'ADALLOM_S_DISCOVERY'
                 ServicePlanFriendlyName = 'CLOUD APP SECURITY DISCOVERY'
-            },
+            }
             [ordered]@{
-                ServicePlanId = '8a256a2b-b617-496d-b51b-e76466e88db0'
-                ServicePlanName = 'MFA_PREMIUM'
+                ServicePlanId           = '8a256a2b-b617-496d-b51b-e76466e88db0'
+                ServicePlanName         = 'MFA_PREMIUM'
                 ServicePlanFriendlyName = 'MICROSOFT AZURE MULTI-FACTOR AUTHENTICATION'
             }
+        )
+    }
+    [ordered]@{
+        SkuId           = '6470687e-a428-4b7a-bef2-8a291ad947c9'
+        SkuPartNumber   = 'WINDOWS_STORE'
+        SkuFriendlyName = 'Unknown'
+        Serviceplans    = @(
+            [ordered]@{
+                ServicePlanId           = '113feb6c-3fe4-4440-bddc-54d774bf0318'
+                ServicePlanName         = 'EXCHANGE_S_FOUNDATION'
+                ServicePlanFriendlyName = ''
+            }
+            [ordered]@{
+                ServicePlanId           = 'a420f25f-a7b3-4ff5-a9d0-5d58f73b537d'
+                ServicePlanName         = 'WINDOWS_STORE'
+                ServicePlanFriendlyName = ''
+            }
+        )
+    }
+    [ordered]@{
+        SkuId           = 'f30db892-07e9-47e9-837c-80727f46fd3d'
+        SkuPartNumber   = 'FLOW_FREE'
+        SkuFriendlyName = 'Microsoft Flow Free'
+        Serviceplans    = @(
+            [ordered]@{
+                ServicePlanId           = '113feb6c-3fe4-4440-bddc-54d774bf0318'
+                ServicePlanName         = 'EXCHANGE_S_FOUNDATION'
+                ServicePlanFriendlyName = ''
+            }
+            [ordered]@{
+                ServicePlanId           = '17ab22cd-a0b3-4536-910a-cb6eb12696c0'
+                ServicePlanName         = 'DYN365_CDS_VIRAL'
+                ServicePlanFriendlyName = ''
+            }
+            [ordered]@{
+                ServicePlanId           = '50e68c76-46c6-4674-81f9-75456511b170'
+                ServicePlanName         = 'FLOW_P2_VIRAL'
+                ServicePlanFriendlyName = ''
+            }
+        )
+    }
+    [ordered]@{
+        SkuId           = '4b9405b0-7788-4568-add1-99614e613b69'
+        SkuPartNumber   = 'EXCHANGESTANDARD'
+        SkuFriendlyName = 'Exchange Online (Plan 1)'
+        Serviceplans    = @(
+
+            [ordered]@{
+                ServicePlanId           = '5e62787c-c316-451f-b873-1d05acd4d12c'
+                ServicePlanName         = 'BPOS_S_TODO_1'
+                ServicePlanFriendlyName = ''
+            }
+            [ordered]@{
+                ServicePlanId           = '882e1d05-acd1-4ccb-8708-6ee03664b117'
+                ServicePlanName         = 'INTUNE_O365'
+                ServicePlanFriendlyName = ''
+            }
+            [ordered]@{
+                ServicePlanId           = '9aaf7827-d63c-4b61-89c3-182f06f82e5c'
+                ServicePlanName         = 'EXCHANGE_S_STANDARD'
+                ServicePlanFriendlyName = ''
+            }
+        )
+    }
+    [ordered]@{
+        SkuId           = '74fbf1bb-47c6-4796-9623-77dc7371723b'
+        SkuPartNumber   = 'MS_TEAMS_IW'
+        SkuFriendlyName = 'Microsoft Teams Trial'
+        Serviceplans    = @(
+
+            [ordered]@{
+                ServicePlanId           = '7547a3fe-08ee-4ccb-b430-5077c5041653'
+                ServicePlanName         = 'YAMMER_ENTERPRISE'
+                ServicePlanFriendlyName = ''
+            }
+            [ordered]@{
+                ServicePlanId           = 'a23b959c-7ce8-4e57-9140-b90eb88a9e97'
+                ServicePlanName         = 'SWAY'
+                ServicePlanFriendlyName = ''
+            }
+            [ordered]@{
+                ServicePlanId           = '902b47e5-dcb2-4fdc-858b-c63a90a2bdb9'
+                ServicePlanName         = 'SHAREPOINTDESKLESS'
+                ServicePlanFriendlyName = ''
+            }
+            [ordered]@{
+                ServicePlanId           = '92f7a6f3-b89b-4bbd-8c30-809e6da5ad1c'
+                ServicePlanName         = 'POWERAPPS_O365_P1'
+                ServicePlanFriendlyName = ''
+            }
+            [ordered]@{
+                ServicePlanId           = 'e95bec33-7c88-4a70-8e19-b10bd9d0c014'
+                ServicePlanName         = 'SHAREPOINTWAC'
+                ServicePlanFriendlyName = ''
+            }
+            [ordered]@{
+                ServicePlanId           = '57ff2da0-773e-42df-b2af-ffb7a2317929'
+                ServicePlanName         = 'TEAMS1'
+                ServicePlanFriendlyName = ''
+            }
+            [ordered]@{
+                ServicePlanId           = '42a3ec34-28ba-46b6-992f-db53a675ac5b'
+                ServicePlanName         = 'MCO_TEAMS_IW'
+                ServicePlanFriendlyName = ''
+            }
+            [ordered]@{
+                ServicePlanId           = 'b737dad2-2f6c-4c65-90e3-ca563267e8b9'
+                ServicePlanName         = 'PROJECTWORKMANAGEMENT'
+                ServicePlanFriendlyName = ''
+            }
+            [ordered]@{
+                ServicePlanId           = '0f9b09cb-62d1-4ff4-9129-43f4996f83f4'
+                ServicePlanName         = 'FLOW_O365_P1'
+                ServicePlanFriendlyName = ''
+            }
+            [ordered]@{
+                ServicePlanId           = '113feb6c-3fe4-4440-bddc-54d774bf0318'
+                ServicePlanName         = 'EXCHANGE_S_FOUNDATION'
+                ServicePlanFriendlyName = ''
+            }
+
         )
     }
 )
 
 $OutPathJson = Join-Path -Path $PWD -ChildPath 'AzureSku.json'
-$Skus | ConvertTo-Json -Depth 3 | Out-File -FilePath $OutPathJson
+$Skus | ConvertTo-Json -Depth 3 | Out-File -FilePath $OutPathJson -Encoding utf8
